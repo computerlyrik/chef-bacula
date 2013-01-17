aa127a65aa618d41542a0c6ba66e7fe411294419
Christian Fischer <sw-dev@computerlyrik.de>
2013-01-17 15:09:12 +0100

    upsized backup space to 50*1G per default

--- attributes/server.rb
+++ attributes/server.rb
@@ -3,5 +3,5 @@ default['bacula']['mysql_user'] = "bacula"
 default['bacula']['dir']['address'] = node['ipaddress']
 
 default['bacula']['volume_size'] = "1G"
-default['bacula']['volume_max'] = 20
+default['bacula']['volume_max'] = 50
 default['bacula']['label_format'] = "BaculaFile"
