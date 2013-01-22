faa65bb4a1d66fcf0f6eb16c22995600352512d9
Christian Fischer <sw-dev@computerlyrik.de>
2013-01-22 14:36:41 +0100

    added operator and mail email address

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
 default['bacula']['user'] = "bacula"
 default['bacula']['group'] = "bacula"
+
+default['bacula']['messages']['mail'] = "bacula@#{node['domain']}"
+default['bacula']['messages']['operator'] = node['bacula']['messages']['mail']
