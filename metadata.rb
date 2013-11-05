140e8146f737ebcfcbcee284305f8efc372d6ae1
Christian Fischer <sw-dev@computerlyrik.de>
2013-11-05 10:53:35 +0100

    release version 1.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs and autoconfigures bacula backup system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.1"
+version          "1.2.0"
 
 %w{ ubuntu debian}.each do |os|
   supports os
